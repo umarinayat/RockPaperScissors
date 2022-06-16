0001c78f045992bea1e481d39dc719125ff1bf62
umar <umarinayat111@gmail.com>
2022-06-16 17:20:29 +0500

    changes in script

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-console.log("working")
-
 const computerPlay = () => {
     const options = ["Rock", "Paper", "Scissors"]
     return options[Math.floor(Math.random() * options.length)]
 }
-console.log(computerPlay())
+
+const playRound = (computerSelection, playerSelection) => {
+    if(computerSelection == playerSelection){
+       return "It's a tie"
+       
+    }
+    else if((computerSelection == "Rock" && playerSelection == "Paper") || (computerSelection == "Scissors" && playerSelection == "Rock") || (computerSelection == "Paper" && playerSelection == "Scissors")){
+        return "Player won"
+    }
+    else if((computerSelection == "Paper" && playerSelection == "Rock") || (computerSelection == "Rock" && playerSelection == "Scissors") || (computerSelection == "Scissors" && playerSelection == "Paper")){
+        return "Computer Wins"
+    }
+}
+
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let result;
+
+    for(let i = 0; i < 5; i++){
+        result = playRound(computerSelection, playerSelection)
+        if(result == "Player won"){
+            playerScore++
+        }
+        else if(result == "Computer Wins"){
+            computerScore++
+        }
+    }
+}
+const computerSelection = computerPlay()
+const playerSelection = "Rock"
+game()
+
